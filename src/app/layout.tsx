324844f7a96b95692c0bf05a2447f1d90c78938e
chinaza-max <mosesogbonna68@gmail.com>
2024-11-11 23:30:35 +0100

    continue development of login screen

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import "./styles/test.scss"
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
